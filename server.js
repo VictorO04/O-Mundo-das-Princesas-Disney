eda6457d6901e02cf46753015a87bae6a656d80b
Victor Oliveira <victorcriri313@gmail.com>
2025-08-22 14:22:46 -0300

    Get todas as princesas em json

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.send(`Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑`);
 });
 
+app.get("/princesas", (req, res) => {
+    res.send(princesas);
+});
+
 app.listen(serverPort, () => {
     console.log(`✨ O servidor das princesas está em: http://localhost:${serverPort}`);
 });
