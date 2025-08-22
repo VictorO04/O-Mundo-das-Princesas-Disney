3da9e6b3cc6691b914a3ef97a578a969b6c6d0bc
Victor Oliveira <victorcriri313@gmail.com>
2025-08-22 14:28:36 -0300

    Get por ID

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.get("/princesas", (req, res) => {
     res.send(princesas);
 });
 
+app.get("/princesas/:id", (req, res) => {
+    let id = req.params.id;
+    id = parseInt(id);
+    const princesa = princesas.find(p => p.id === id);
+
+    if (princesa) {
+        res.status(200).json(princesa);
+    } else {
+        res.status(404).json({
+            erro: `Princesa com o ID ${id} não encontrada!`
+        });
+    }
+});
+
 app.listen(serverPort, () => {
     console.log(`✨ O servidor das princesas está em: http://localhost:${serverPort}`);
 });
