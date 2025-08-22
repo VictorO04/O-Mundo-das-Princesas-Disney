5a9e4a82fb313c405054890e09c4875443fe7f43
Victor Oliveira <victorcriri313@gmail.com>
2025-08-22 15:53:04 -0300

    adição html

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const serverPort = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(`Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑`);
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Princesas da Disney</title>
+        </head>
+        <body style="background-color: rgba(254, 217, 248, 1);">
+            <h1 style="text-align: center;">Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑</h1>
+            <img src="./src/img/castelo.png" alt="">
+        </body>
+        </html>`);
 });
 
 app.get("/princesas", (req, res) => {
