9f1014f59892e9236005e59f8d222b76e60bc6b3
Victor Oliveira <victorcriri313@gmail.com>
2025-08-22 14:20:45 -0300

    Get página inicial

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import princesas from "./src/data/princesas.js";
+
+const app = express();
+const serverPort = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑`);
+});
+
+app.listen(serverPort, () => {
+    console.log(`✨ O servidor das princesas está em: http://localhost:${serverPort}`);
+});
